2d9b8c8da3c51e418373a755cb3f83a8bb578258
Gastech99 <diabygassamad16@gmail.com>
2024-07-05 00:02:09 +0000

    Les valeurs primitive en Anglais

---
+++ index.js
@@ -0,0 +1,8 @@
+var toto = "Sidi va à l'école";
+console.log(toto);
+// Les types primitives en TypeScript
+var age = 20;
+var firsName = "Djimo";
+var nullablee = null;
+var undec = undefined;
+var isClosed = true;
---
+++ index.ts
@@ -0,0 +1,9 @@
+let toto = "Sidi va à l'école";
+console.log(toto);
+
+// Les types primitives en TypeScript
+const age: number = 20;
+const firsName: string = "Djimo";
+const nullablee: null = null;
+const undec = undefined;                     
+const isClosed: boolean = true;
